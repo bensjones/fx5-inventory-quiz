0aa5ba2c59e21c40cfb898c4067015cb7cd8e5db
Ben Jones <jonesr79@gmail.com>
2020-11-14 10:17:03 -0800

    Add basic dashboard and quiz components

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Welcome to Five X 5 Inventory 101 Quiz</h1>
+        <div class="left">
+            <button @click="startQuiz">Take Quiz</button>
+        </div>
+        <div id="quiz" v-show="testing">
+            <quiz-engine />
+        </div>
+    </div>
+</template>
+
+<script>
+import QuizEngine from './QuizEngine.vue'
+    export default {
+  components: { QuizEngine },
+        name: 'Dashboard',
+        data() {
+            return{
+                history: [],
+                testing: false
+            }
+        },
+        methods: {
+            startQuiz() {
+                this.testing = !this.testing
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/QuizEngine.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        Hello World
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'quizEngine',
+        data() {
+            return {
+                questions: [],
+                currentIndex: 0,
+                correctCount: 0,
+                results: undefined
+            }
+        },
+        methods: {
+            start() {
+
+            },
+            checkAnswer() {
+
+            },
+            nextQuestion() {
+
+            },
+            showResults() {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
